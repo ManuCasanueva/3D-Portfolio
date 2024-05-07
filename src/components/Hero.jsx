bddc3a26642f91cf976e6d947def887bbd3b5a26
manuel Casanueva <manucasanueva3@gmail.com>
2024-05-07 11:45:35 +0200

    Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
-
 import { motion } from "framer-motion";
+import { useMediaQuery } from 'react-responsive';
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
 
-  
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
@@ -29,26 +29,25 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {isDesktop && <ComputersCanvas />}
 
       <div className='absolute xs:bottom-16 bottom-32 w-full flex justify-center items-center'>
-  <a href='#about'>
-    <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-      <motion.div
-        animate={{
-          y: [0, 24, 0],
-        }}
-        transition={{
-          duration: 1.5,
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
-        className='w-3 h-3 rounded-full bg-secondary mb-1'
-      />
-    </div>
-  </a>
-</div>
-
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
